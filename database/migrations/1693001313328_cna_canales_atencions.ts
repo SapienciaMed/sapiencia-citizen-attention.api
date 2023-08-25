f1a739b47e0b5ca29000831206c863a7b5a4a9bd
Jhon Camilo Moreno <jcmoreno@i4digital.com>
2023-08-25 17:23:05 -0500

    migración tabla cna_canales_atencion

---
+++ database/migrations/1693001313328_cna_canales_atencions.ts
@@ -0,0 +1,27 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'CNA_CANALES_ATENCION'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+
+      table.increments('CNA_CODIGO')
+        .notNullable()
+        .comment('LLave primaria');
+
+      table.string('CNA_CANAL',50).notNullable();
+
+      table.boolean('CNA_ACTIVO')
+        .notNullable()
+        .defaultTo(true);
+
+      table.integer('CNA_ORDEN').notNullable();
+
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
