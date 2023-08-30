87329d0b3cd5a226219504ec69596b4fa2abc990
Jhon Camilo Moreno <jcmoreno@i4digital.com>
2023-08-30 10:16:48 -0500

    modelos seeders

--- app/Models/PrgPrograma.ts
+++ app/Models/PrgPrograma.ts
@@ -1,13 +1,36 @@
-import { DateTime } from 'luxon'
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { BaseModel, HasMany, column, hasMany } from '@ioc:Adonis/Lucid/Orm'
+import ClpClasificacionPrograma from './ClpClasificacionPrograma';
+import DepDependencia from './DepDependencia';
 
 export default class PrgPrograma extends BaseModel {
-  @column({ isPrimary: true })
-  public id: number
+  
+  public static table = 'PRG_PROGRAMAS';
 
-  @column.dateTime({ autoCreate: true })
-  public createdAt: DateTime
+  @column({ isPrimary: true, columnName:'PRG_CODIGO' })
+  public prg_codigo: number;
 
-  @column.dateTime({ autoCreate: true, autoUpdate: true })
-  public updatedAt: DateTime
+  @column({ columnName:'PRG_DESCRIPCION', serializeAs:'' })
+  public prg_descripcion: string;
+
+  @hasMany(() => ClpClasificacionPrograma, {
+    localKey: 'prg_codigo',
+    foreignKey: 'clp_codigo',
+    serializeAs:'clpClasificacionPrograma'
+  })
+  public clpClasificacionPrograma: HasMany<typeof ClpClasificacionPrograma>;
+
+  @hasMany(() => DepDependencia, {
+    localKey: 'prg_codigo',
+    foreignKey: 'dep_codigo',
+    serializeAs: 'depDependencia'
+  })
+  public depDependencia: HasMany<typeof DepDependencia>;
+
+  @column({ columnName:'PRG_ACTIVO', serializeAs:'prg_activo' })
+  public prg_activo: boolean;
+
+  @column({ columnName:'PRG_ORDEN', serializeAs:'prg_orden' })
+  public prg_orden: number;
+
+
 }
--- database/seeders/PrgPrograma.ts
+++ database/seeders/PrgPrograma.ts
@@ -1,7 +1,62 @@
 import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
+import PrgPrograma from 'App/Models/PrgPrograma'
 
 export default class extends BaseSeeder {
   public async run () {
     // Write your database queries inside the run method
+
+    await PrgPrograma.createMany([
+
+      {
+        prg_descripcion: 'Becas Mejores Bachilleres',
+        prg_orden:1,
+      },
+
+      {
+        prg_descripcion: 'Becas Mejores Deportistas',
+        prg_orden:2
+      },
+
+      {
+        prg_descripcion: 'Enlaza Mundos',
+        prg_orden:3
+      },
+
+      {
+        prg_descripcion: 'Formación Avanzada',
+        prg_orden:4
+      },
+
+      {
+        prg_descripcion: 'Extendiendo Fronteras Educativas',
+        prg_orden:5
+      },
+
+      {
+        prg_descripcion: 'EPM',
+        prg_orden:6
+      },
+
+      {
+        prg_descripcion: 'Presupuesto Participativo',
+        prg_orden:7
+      },
+
+      {
+        prg_descripcion: 'Becas Tecnologías',
+        prg_orden:8
+      },
+
+      {
+        prg_descripcion: 'Matrícula Cero',
+        prg_orden:9
+      },
+
+      {
+        prg_descripcion: 'Talento Especializado',
+        prg_orden:10
+      },
+
+    ]);
   }
 }
