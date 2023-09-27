63f2665f66baf4847e8162d55a507cde5ea66df3
Juan Pablo Madariaga <juanpablomadariagacardona@gmail.com>
2023-09-27 12:43:49 -0500

    [entidades de trabajo]

--- app/Services/External/AuthExternalService.ts
+++ app/Services/External/AuthExternalService.ts
@@ -2,17 +2,26 @@ import { IUser, IUserFilters } from "App/Interfaces/UserInterfaces";
 import { ApiResponse, IPagingData } from "App/Utils/ApiResponses";
 import axios, { AxiosInstance } from "axios";
 import { IAuthExternalService } from "./Contracts/IAuthExternalService";
+import HttpContext from '@ioc:Adonis/Core/HttpContext'
 
 export default class AuthExternalService implements IAuthExternalService {
   private apiAuth: AxiosInstance;
 
   constructor() {
+    const ctx = HttpContext.get();
     this.apiAuth = axios.create({
       baseURL: process.env.APP_API_AUTH,
+      headers: {
+        "Content-Type": "application/json",
+          Accept: "application/json",
+        permissions: ctx?.request.header('permissions'),
+        Authorization: ctx?.request.header('Authorization'),
+      }
     });
   }
   public async getUserByDocument(identification: string): Promise<ApiResponse<IUser|null>> {
-    const items = await this.apiAuth.get<ApiResponse<IUser|null>>(`user/get-by-document/${identification}`);
+
+    const items = await this.apiAuth.get<ApiResponse<IUser|null>>(`user/get-by-document/${identification}`)
 
     return items.data;
   }
