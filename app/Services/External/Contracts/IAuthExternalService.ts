5fe98ad02ee0b6fc37da167802b9bfcaae1ea386
Juan Pablo Madariaga <juanpablomadariagacardona@gmail.com>
2023-10-03 10:58:26 -0500

    [Entidades de trabajo]

--- app/Services/External/Contracts/IAuthExternalService.ts
+++ app/Services/External/Contracts/IAuthExternalService.ts
@@ -3,6 +3,7 @@ import { ApiResponse, IPagingData } from "App/Utils/ApiResponses";
 
 export interface IAuthExternalService {
   getUserById(id: number): Promise<ApiResponse<IUser | null>>;
+  getUsersByIds(ids: number[]): Promise<ApiResponse<IUser[]>>;
   getUserByDocument(identification: string): Promise<ApiResponse<IUser | null>>;
   searchUser(filter: IUserFilters): Promise<ApiResponse<IPagingData<IUser | null>>>;
 }
