d2f9603f0b93edd6fe98dda68e875b96a22bc054
Juan Pablo Madariaga <juanpablomadariagacardona@gmail.com>
2023-10-04 10:10:35 -0500

    [Entidades de trabajo]

---
+++ app/Models/EntityAffairsProgram.ts
@@ -0,0 +1,50 @@
+import { DateTime } from 'luxon'
+import { BaseModel, BelongsTo, belongsTo, column } from '@ioc:Adonis/Lucid/Orm'
+import AffairsProgram from './AffairsProgram';
+import WorkEntity from './WorkEntity';
+
+export default class EntityAffairsProgram extends BaseModel {
+
+
+
+
+
+
+public static table = "EPA_ENTIDAD_PROGRAMA_ASUNTO";
+
+@column({ isPrimary: true, columnName: "EPA_CODIGO", serializeAs: "id" })
+public id: number;
+
+@column({ columnName: "EPA_CODENT_ENTIDAD_TRABAJO", serializeAs: "workEntityId" })
+public workEntityId: number;
+
+@column({ columnName: "EPA_CODPRA_PROGRAMA_ASUNTO", serializeAs: "affairsProgramId" })
+public affairsProgramId: number;
+
+@belongsTo(() => WorkEntity, {
+  localKey: "id",
+  foreignKey: "workEntityId",
+})
+public workEntity: BelongsTo<typeof WorkEntity>;
+
+@belongsTo(() => AffairsProgram, {
+  localKey: "id",
+  foreignKey: "affairsProgramId",
+})
+public affairsProgram: BelongsTo<typeof AffairsProgram>;
+
+@column.dateTime({
+  autoCreate: true,
+  columnName: "EPA_FECHA_CREACION",
+  serializeAs: "createdAt",
+})
+public createdAt: DateTime;
+
+@column.dateTime({
+  autoCreate: true,
+  autoUpdate: true,
+  columnName: "EPA_FECHA_ACTUALIZACION",
+  serializeAs: "updatedAt",
+})
+public updatedAt: DateTime;
+}
---
+++ database/migrations/1696431788425_entity_affairs_programs.ts
@@ -0,0 +1,31 @@
+import BaseSchema from "@ioc:Adonis/Lucid/Schema";
+
+export default class extends BaseSchema {
+  protected tableName = "EPA_ENTIDAD_PROGRAMA_ASUNTO";
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments("EPA_CODIGO");
+      table
+        .integer("EPA_CODENT_ENTIDAD_TRABAJO")
+        .unsigned()
+        .references("ENT_ENTIDAD_TRABAJO.ENT_CODIGO")
+        .comment("Llave foranea a la tabla entidad de trabajo(FK_ENT_CODIGO)");
+      table
+        .integer("EPA_CODPRA_PROGRAMA_ASUNTO")
+        .unsigned()
+        .references("PRA_PROGRAMA_ASUNTOS.PRA_CODIGO")
+        .comment("Llave foranea a la tabla programa asuntos(FK_PRA_CODIGO)");
+
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp("EPA_FECHA_CREACION", { useTz: true }).defaultTo(this.now());
+      table.timestamp("EPA_FECHA_ACTUALIZACION", { useTz: true }).nullable();
+    });
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName);
+  }
+}
